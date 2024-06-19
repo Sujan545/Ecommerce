2b0f3e09482e70a2b3341d40a2af879762191d6a
Sujan545 <ksuzan545@gmail.com>
2024-06-19 19:03:08 +0545

    comit

--- client/src/components/Navbar/DarkMode.jsx
+++ client/src/components/Navbar/DarkMode.jsx
@@ -1,23 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import LightButton from "../../assets/website/light-mode-button.png";
 import DarktButton from "../../assets/website/dark-mode-button.png";
 
 
 const DarkMode = () => {
 
-    const [theme, setTheme] = useState("dark")
+    const [theme, setTheme] = useState(
+        localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+    );
+    const element=document.documentElement;
+    console.log(element);
+    useEffect(()=>{
+        localStorage.setItem("theme",theme);
+        if(theme==="dark"){
+            element.classList.add("dark");
+            element.classList.add("dark");
+        }else{
+            element.classList.add("light");
+            element.classList.add("light");
+        }
+    });
 
     return (
         <div className='relative'>
             <img
                 onClick={() => setTheme(theme === "dark" ? "light" : "dark")} src={LightButton}
                 alt="" className={`w-12 cursor-pointer absolute right-0 z-10
-        ${theme === "dark" ? "opacity-0" : "opacity-100"}`}
+        ${theme === "dark" ? "opacity-0" : "opacity-100"} transition-all duration-300`}
             />
             <img
-                onClick={() => setTheme(theme === "dark" ? "light" : "dark")} src={LightButton} src={DarktButton}
-                alt="" className={`w-12 cursor-pointer
-        `}
+                onClick={() => setTheme(theme === "dark" ? "light" : "dark")} src={DarktButton} 
+                alt="" className={`w-12 cursor-pointer `}
             />
 
         </div>
