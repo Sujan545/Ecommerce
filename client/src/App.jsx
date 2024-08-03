4f674925c1b5586b0bddd94cc000b0864858d3be
Sujan545 <ksuzan545@gmail.com>
2024-08-03 08:46:31 +0545

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Category from './components/category/Category'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Category />
     </div>
   )
 }
