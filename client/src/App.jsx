c9d85bb9ed11cd1fff00a93051e7f62f2a21ea60
Sujan545 <ksuzan545@gmail.com>
2024-08-04 13:35:19 +0545

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Category from './components/category/Category'
+import Category2 from './components/category/Category2'
+import Services from './components/services/Services'
 
 const App = () => {
   return (
-    <div>
+    <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
       <Navbar />
       <Hero />
       <Category />
+      <Category2 />
+      <Services />
     </div>
   )
 }
