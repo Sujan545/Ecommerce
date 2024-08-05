c17b845cbf73d794471857ee1a5f8530a28ba00a
Sujan545 <ksuzan545@gmail.com>
2024-08-05 15:43:26 +0545

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Category from './components/category/Category'
@@ -8,6 +8,10 @@ import Banner from './components/Banner/Banner'
 import headphone from './assets/hero/headphone.png'
 import smartwatch2 from './assets/category/smartwatch2.png'
 import Product from './components/Product/Product'
+import Blogs from './components/Blogs/Blogs'
+import Partner from './components/Partner/Partner'
+import Footer from './components/Footer/Footer'
+import Popup from './components/Popup/Popup'
 
 const BannerData = {
   discount: "30% OFF",
@@ -33,16 +37,24 @@ const BannerData2 = {
 
 
 const App = () => {
+  const [orderPopup,setOrderPopup]=useState(false);
+  const handleOrderPopup=()=>{
+setOrderPopup(!orderPopup);
+  }
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
-      <Navbar />
-      <Hero />
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero handleOrderPopup={handleOrderPopup}/>
       <Category />
       <Category2 />
       <Services />
       <Banner data={BannerData} />
       <Product />
       <Banner data={BannerData2} />
+      <Blogs />
+      <Partner />
+      <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
   )
 }
